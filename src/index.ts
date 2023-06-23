ae9dcccc5284a003c8afa2911182cbce3bfac45d
Neil Murphy <barrymun@tcd.ie>
2023-06-23 22:38:57 +0100

    reformat

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
 import * as THREE from 'three';
 
 function main() {
-  const canvas = document.querySelector('#c') as HTMLCanvasElement;
-  const renderer = new THREE.WebGLRenderer({ canvas });
+    const canvas = document.querySelector('#c') as HTMLCanvasElement;
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-  const fov = 75;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
+    const fov = 75;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
 
-  const scene = new THREE.Scene();
+    const scene = new THREE.Scene();
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-  function render(time: number) {
-    time *= 0.001;  // convert time to seconds
+    function render(time: number) {
+        time *= 0.001; // convert time to seconds
 
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+        cube.rotation.x = time;
+        cube.rotation.y = time;
 
-    renderer.render(scene, camera);
+        renderer.render(scene, camera);
 
+        requestAnimationFrame(render);
+    }
     requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
 }
 
 main();
